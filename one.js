bf16eb775898469544bb531e75a8a8c52296097d
Amit Chanchal <amit.chanchal@hotmail.com>
2017-11-30 14:04:50 +0530

    new window from renderer

--- one.js
+++ one.js
@@ -1,1 +1,15 @@
 console.log('from renderer one');
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+const newWindowButton = document.getElementById('winThree');
+newWindowButton.addEventListener('click', function(event) {
+    let winThree = new BrowserWindow();
+    // Loading an html file in the window
+    winThree.loadURL(url.format({
+        pathname: path.join(__dirname, 'three.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
