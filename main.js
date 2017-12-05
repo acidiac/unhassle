79cad78bd177bf0d31b4bb77c7c2b1e816467e61
Amit Chanchal <amit.chanchal@hotmail.com>
2017-12-05 14:40:19 +0530

    main page

--- main.js
+++ main.js
@@ -5,38 +5,31 @@ const path = require('path');
 const url = require("url");
 
 //declaring a window
-let winOne, winTwo;
+let win;
 
 // just testing
 console.log("from the main process");
 
 function createWindow() {
     // new browser window instance
-    winOne = new BrowserWindow();
-    winTwo = new BrowserWindow();
+    win = new BrowserWindow();
+
     // Loading an html file in the window
-    winOne.loadURL(url.format({
-        pathname: path.join(__dirname, 'one.html'),
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
 
-    winTwo.loadURL(url.format({
-        pathname: path.join(__dirname, 'two.html'),
-        protocol: 'file',
-        slashes: true
-    }));
+
 
     // Dev console
-    winOne.webContents.openDevTools();
-    winTwo.webContents.openDevTools();
+    win.webContents.openDevTools();
     //handling when user closes the window
-    winOne.on('closed', () => {
+    win.on('closed', () => {
         win = null;
     });
-    winTwo.on('closed', () => {
-        win = null;
-    });
+
 }
 
 app.on('ready', createWindow);
